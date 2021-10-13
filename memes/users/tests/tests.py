565cb49704318404431c6d73fc712eab9ec47bd2
p2vvel <p2vvel@gmail.com>
2021-10-13 18:59:30 +0200

    Rewritten tests and urls

--- memes/users/tests/tests.py
+++ memes/users/tests/tests.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 # Create your tests here.
 
 
-class LoginTests(TestCase):
+class UserTests(TestCase):
     def setUp(self):
         user_model = get_user_model()
         new_user = user_model(login="jerry", email="jerry@example.com")
@@ -18,11 +18,17 @@ class LoginTests(TestCase):
         '''Is login system working?'''
         self.assertTrue(self.client.login(email="jerry@example.com", password="1234"))
 
+    def test_login_view(self):
+        '''Does login view work'''
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
     def test_logout(self):
         '''Does logging out works?'''
         self.assertTrue(self.client.login(email="jerry@example.com", password="1234"))
         response = self.client.get(reverse("logout"))
         user = get_user(self.client)
+        self.assertRedirects(response, reverse("index"))    #checks redirect after logout
         self.assertFalse(user.is_authenticated)
 
     def test_password_change_view_anonymous(self):
@@ -30,7 +36,17 @@ class LoginTests(TestCase):
         response = self.client.get(reverse("password_change"), follow=True)
         self.assertRedirects(response, expected_url="{}?next={}".format(reverse("login"), reverse("password_change")))
 
+    def test_password_change_view_logged(self):
+        '''Does signed in user see password change view?'''
+        self.client.login(email="jerry@example.com", password="1234")
+        response = self.client.get(reverse("password_change"))
+        self.assertEqual(response.status_code, 200)
+
     def test_login_redirect(self):
         '''Logged user shouldnt be able to visit login page'''
-        response = self.client.get(reverse("login"))
-        self.assertRedirects(response, reverse("index"), )
+        self.client.login(email="jerry@example.com", password="1234")
+        response = self.client.get(reverse("login"), follow=True)
+        # self.assertRedirects(response, reverse("index"), )
+        self.assertRedirects(response, expected_url=reverse("my_profile"))
+
+    
--- memes/users/urls.py
+++ memes/users/urls.py
@@ -17,6 +17,7 @@ from django.http.response import HttpResponse
 from django.urls import path
 from django.urls.conf import include
 from .views import UserProfileView, my_profile, signup_view
+from django.contrib.auth import views as auth_views
 
 # app_name = "users"    #problem with redirections with default auth views when app_name is set
 
@@ -24,6 +25,8 @@ from .views import UserProfileView, my_profile, signup_view
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path("profile/", my_profile, name="my_profile"),
-    # path("profile/", UserProfileView.as_view(), "user_profile"),
-    path("", include("django.contrib.auth.urls")),
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name="users/login.html"), name="login"),
+    path("password_change/", auth_views.PasswordChangeView.as_view(template_name="users/password_change_form.html"), name="password_change"),
+    path("password_change_done/", auth_views.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
